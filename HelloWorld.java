6f509eadea0a0e4d1fe5988ef7d9195bf25c6444
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-18 19:29:12 +0100

    add main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 # demo-git-commit
 
 public class HelloWorld {
-public static void main(String[] args){
-User user = new User():
-user.name = "Alex";
-user.password = "345234";
-}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        User user = new User():
+        user.name = "Alex";
+        user.password = "345234";
+    }
 }
